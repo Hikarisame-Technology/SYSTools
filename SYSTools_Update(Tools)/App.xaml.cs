13baafeda792b787478310c588a9c99d79df37eb
ShizukuNanami <129707817+Shizuku-Nanami@users.noreply.github.com>
2023-04-22 15:51:13 +0800

    升级工具项目添加

---
+++ SYSTools_Update(Tools)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SYSTools_Update_Tools_
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
