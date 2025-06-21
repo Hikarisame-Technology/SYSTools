51eed3fbe376529d6b258c481b6ef56d55ea28fe
ShizukuNanami <129707817+Shizuku-Nanami@users.noreply.github.com>
2025-06-21 23:14:58 +0800

    修改多语言切换逻辑

---
+++ SYSTools/Helpers/LocalizationManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using SYSTools.Properties;
+using System.Windows.Markup;
+using System.Windows.Data;
+
+namespace SYSTools.Helpers
+{
+    public class LocalizationManager : INotifyPropertyChanged
+    {
+        private LocalizationManager() { }
+
+        public static LocalizationManager Instance { get; } = new LocalizationManager();
+
+        private CultureInfo _currentCulture = CultureInfo.CurrentUICulture;
+        public CultureInfo CurrentCulture
+        {
+            get => _currentCulture;
+            set
+            {
+                if (_currentCulture != value)
+                {
+                    _currentCulture = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
+                }
+            }
+        }
+
+        public string this[string key] =>
+            Lang.ResourceManager.GetString(key, _currentCulture) ?? string.Empty;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+    
+} 
--- SYSTools/Model/ResourceExtension.cs
+++ SYSTools/Model/ResourceExtension.cs
@@ -1,61 +1,27 @@
 ﻿using System;
-using System.Globalization;
 using System.Windows.Markup;
-using System.Windows;
 using System.Windows.Data;
+using SYSTools.Helpers;
 
 namespace SYSTools.Model
 {
     public class ResourceExtension : MarkupExtension
     {
-        private static event EventHandler LanguageChanged;
-        private object _targetObject;
-        private object _targetProperty;
-        
         public string Key { get; set; }
-
-        public static void NotifyLanguageChanged()
-        {
-            LanguageChanged?.Invoke(null, EventArgs.Empty);
-        }
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
+
             if (string.IsNullOrEmpty(Key))
                 return string.Empty;
 
-            // 保存目标对象和属性
-            var provideValueTarget = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
-            if (provideValueTarget != null)
+            // 返回一个绑定到 LocalizationManager 的动态资源
+            var binding = new Binding($"[{Key}]")
             {
-                _targetObject = provideValueTarget.TargetObject;
-                _targetProperty = provideValueTarget.TargetProperty;
-            }
-
-            // 订阅语言变更事件
-            LanguageChanged += OnLanguageChanged;
-
-            return GetResourceString();
-        }
-
-        private void OnLanguageChanged(object sender, EventArgs e)
-        {
-            if (_targetObject != null && _targetProperty != null)
-            {
-                // 在UI线程上更新值
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    if (_targetObject is DependencyObject targetObject && _targetProperty is DependencyProperty targetProperty)
-                    {
-                        targetObject.SetValue(targetProperty, GetResourceString());
-                    }
-                });
-            }
-        }
-
-        private string GetResourceString()
-        {
-            return Properties.Lang.ResourceManager.GetString(Key, CultureInfo.CurrentUICulture) ?? string.Empty;
+                Source = LocalizationManager.Instance,
+                Mode = BindingMode.OneWay
+            };
+            return binding.ProvideValue(serviceProvider);
         }
     }
 }
